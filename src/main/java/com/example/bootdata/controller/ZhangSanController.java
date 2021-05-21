20ec5a49d9ae6858352bfa1763bfc7637ab86d0b
ken <ken@ken.com>
2021-05-21 16:39:31 +0800

    张三开发员提交

---
+++ src/main/java/com/example/bootdata/controller/ZhangSanController.java
@@ -0,0 +1,11 @@
+package com.example.bootdata.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * user: ken
+ * data: 2021/5/21 16:38
+ */
+@Controller
+public class ZhangSanController {
+}
